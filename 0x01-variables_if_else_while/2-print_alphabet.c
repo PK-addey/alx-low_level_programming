0e34d083494f814ad475d666fefb492ffa4a0b3c
Pk-addey <pkaddey@gmail.com>
2024-06-29 15:19:00 +0000

    Added a program that prints the alphabet in lowercase, followed by a new line 'using putchar'.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* return: Always 0 (success)
+*/
+int main(void)
+{
+int c;
+for (c = 'a'; c <= 'z';c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
