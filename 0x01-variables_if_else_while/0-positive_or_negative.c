3392a1a3695f3a740682dd6644967d9930ac1f17
Pk-addey <pkaddey@gmail.com>
2024-06-29 12:38:12 +0000

    Modified script and removed whitespaces.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,33 +3,30 @@
 #include <time.h>
 
 /**
- * main - Entry point, assigns a random number to n and prints its sign
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point, assigns a random number to n and prints its sign
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    int n;
+int n;
 
-    srand(time(0));  
-    n = rand() - RAND_MAX / 2;
+srand(time(0));  
+n = rand() - RAND_MAX / 2;
 
-    /* Print the generated number */
-    printf("%d\n", n);
+/* Determine and print the sign of the number */
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else
+{
+printf("%d is zero\n", n);
+}
 
-    /* Determine and print the sign of the number */
-    if (n > 0)
-    {
-        printf("%d is positive\n", n);
-    }
-    else if (n < 0)
-    {
-        printf("%d is negative\n", n);
-    }
-    else
-    {
-        printf("%d is zero\n", n);
-    }
-
-    return (0);
+return (0);
 }
