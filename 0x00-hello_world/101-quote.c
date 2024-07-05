2c840b58601112ffd92fba8d5d6d64e0e178948f
Pk-addey <pkaddey@gmail.com>
2024-07-05 17:51:14 +0000

    Added a program that prints a string.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+const char *quote = "Exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fwrite(quote, strlen(quote), 1, stderr);
+return 1;
+}
