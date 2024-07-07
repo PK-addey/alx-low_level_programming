14221c20144d7a977b6f80d7a516ef0073c06d68
Pk-addey <pkaddey@gmail.com>
2024-07-07 10:14:32 +0000

    Added aprogram that prints all possible different combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,10 +15,14 @@ for (j = i + 1; j < 10; j++)
 {
 putchar(i + '0');
 putchar(j + '0');
+if (i == 8 && j == 9)
+putchar('\n');
+else
+{
 putchar(',');
 putchar(' ');
 }
 }
-putchar('\n');
+}
 return (0);
 }
