336a5e6f09fed80cf627e1afad28e64241ff598a
Pk-addey <pkaddey@gmail.com>
2024-07-07 16:33:06 +0000

    Modified program.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,20 +6,44 @@
 * Return: Always 0 (Success)
 */
 int main(void)
+{
+unsigned long a = 1, b = 2;
+unsigned long a_head, a_tail, b_head, b_tail;
+unsigned long split = 1000000000;
+int count;
 
+unsigned long next_head;
+unsigned long next_tail;
+
+printf("%lu, %lu", a, b);
+
+for (count = 2; count < 98; count++)
 {
-int a = 1;
-int b = 2;
-int i;
-int next;
-printf("%d, %d", a, b);
-for (i = 3; i <= 98; i++)
+if (b > split / 10)
 {
-next = a + b;
-printf(", %d", next);
+a_head = a / split;
+a_tail = a % split;
+b_head = b / split;
+b_tail = b % split;
+
+next_head = a_head + b_head + ((a_tail + b_tail) / split);
+next_tail = (a_tail + b_tail) % split;
+printf(", %lu%09lu", next_head, next_tail);
+
+a_head = b_head;
+a_tail = b_tail;
+b_head = next_head;
+b_tail = next_tail;
+}
+else
+{
+unsigned long next = a + b;
+printf(", %lu", next);
 a = b;
 b = next;
 }
+}
+
 printf("\n");
 return (0);
 }
