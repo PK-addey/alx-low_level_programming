41f8814bc818bcd7ee326aca76ce5d478bff02c9
Pk-addey <pkaddey@gmail.com>
2024-07-07 17:05:29 +0000

    Modified code.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,43 +7,38 @@
 */
 int main(void)
 {
-unsigned long a = 1, b = 2;
-unsigned long a_head, a_tail, b_head, b_tail;
-unsigned long split = 1000000000;
 int count;
-
-unsigned long next_head;
-unsigned long next_tail;
-
-printf("%lu, %lu", a, b);
-
-for (count = 2; count < 98; count++)
+unsigned long fib1 = 0, fib2 = 1, sum;
+unsigned long fib1_half1, fib1_half2, fib2_half1,fib2_half2;
+unsigned long half1,half2;
+for (count = 0; count < 92; count++)
 {
-if (b > split / 10)
+sum = fib1 +fib2;
+printf("%lu, ", sum);
+fib1 = fib2;
+fib2 =sum;
+}
+fib1_half1 = fib1 / 10000000000;
+fib2_half1 = fib2 / 10000000000;
+fib1_half2 = fib1 / 10000000000;
+fib2_half2 = fib2 / 10000000000;
+for (count = 93; count < 99; count++)
 {
-a_head = a / split;
-a_tail = a % split;
-b_head = b / split;
-b_tail = b % split;
-
-next_head = a_head + b_head + ((a_tail + b_tail) / split);
-next_tail = (a_tail + b_tail) % split;
-printf(", %lu%09lu", next_head, next_tail);
-
-a_head = b_head;
-a_tail = b_tail;
-b_head = next_head;
-b_tail = next_tail;
-}
-else
+half1 = fib1_half1 + fib2_half1;
+half2 = fib1_half2 + fib2_half2;
+if (fib1_half2 + fib2_half2 > 9999999999)
 {
-unsigned long next = a + b;
-printf(", %lu", next);
-a = b;
-b = next;
+half1 += 1;
+half2 %= 10000000000;
 }
+printf("%lu%lu", half1, half2);
+if (count != 98)
+printf(", ");
+fib1_half1 = fib2_half1;
+fib1_half2 = fib2_half2;
+fib2_half1 = half1;
+fib2_half2 = half2;
 }
-
 printf("\n");
 return (0);
 }
