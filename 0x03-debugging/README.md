21f073eda5c2ea651f8dd2477048261fe01bbbb0
Pk-addey <pkaddey@gmail.com>
2024-07-09 00:08:15 +0000

    Tested the 0-main.c function.

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+# C - Debugging
+
+Welcome to the "C - Debugging" project. This project focuses on learning and applying debugging techniques to identify and fix errors in C programs. Debugging is an essential skill for any programmer, and this project will help you develop the ability to diagnose and resolve issues in your code.
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - Checks if a number is positive or negative
+ * @i: The number to be checked
+ *
+ * This function prints whether the number stored in i is positive,
+ * negative, or zero.
+ */
+void positive_or_negative(int i)
+{
+    if (i > 0)
+    {
+        printf("%d is positive\n", i);
+    }
+    else if (i < 0)
+    {
+        printf("%d is negative\n", i);
+    }
+    else
+    {
+        printf("%d is zero\n", i);
+    }
+}
