eba1794d0f960ef51268a04d969dba764b75f08b
Pk-addey <pkaddey@gmail.com>
2024-12-21 11:33:15 +0000

    Corrected files.

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,53 +1,52 @@
 #include "main.h"
 
 /**
-* convert_day - converts day of month to day of year, accounting for leap years
+* convert_day - converts day of month to day of year, without accounting
+* for leap year
 * @month: month in number format
 * @day: day of month
-* @is_leap_year: 1 if the year is a leap year, 0 otherwise
 * Return: day of year
 */
 
-int convert_day(int month, int day, int is_leap_year)
+int convert_day(int month, int day)
 {
-switch (month)
-{
-case 2:
-day = 31 + day;
-break;
-case 3:
-day = 59 + day + is_leap_year;
-break;
-case 4:
-day = 90 + day + is_leap_year;
-break;
-case 5:
-day = 120 + day + is_leap_year;
-break;
-case 6:
-day = 151 + day + is_leap_year;
-break;
-case 7:
-day = 181 + day + is_leap_year;
-break;
-case 8:
-day = 212 + day + is_leap_year;
-break;
-case 9:
-day = 243 + day + is_leap_year;
-break;
-case 10:
-day = 273 + day + is_leap_year;
-break;
-case 11:
-day = 304 + day + is_leap_year;
-break;
-case 12:
-day = 334 + day + is_leap_year;
-break;
-default:
-return -1;
-}
-
-return day;
+    switch (month)
+    {
+        case 2:
+            day = 31 + day;
+            break;
+        case 3:
+            day = 59 + day;
+            break;
+        case 4:
+            day = 90 + day;
+            break;
+        case 5:
+            day = 120 + day;
+            break;
+        case 6:
+            day = 151 + day;
+            break;
+        case 7:
+            day = 181 + day;
+            break;
+        case 8:
+            day = 212 + day;
+            break;
+        case 9:
+            day = 243 + day;
+            break;
+        case 10:
+            day = 273 + day;
+            break;
+        case 11:
+            day = 304 + day;
+            break;
+        case 12:
+            day = 334 + day;
+            break;
+        default:
+            break;
+    }
+    return (day);
 }
