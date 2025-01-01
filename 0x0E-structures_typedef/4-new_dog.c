d1f146ed8a2a01b4bf19ec4210732db5aa4b368c
Pk-addey <pkaddey@gmail.com>
2025-01-01 19:17:07 +0000

    Added a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,27 +14,36 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *dog;
 char *name_copy, *owner_copy;
+size_t name_len = 0, owner_len = 0;
+size_t i;
+
+while (name[name_len])
+name_len++;
+while (owner[owner_len])
+owner_len++;
 
 dog = malloc(sizeof(dog_t));
 if (dog == NULL)
 return (NULL);
 
-name_copy = malloc(strlen(name) + 1);
+name_copy = malloc(name_len + 1);
 if (name_copy == NULL)
 {
 free(dog);
 return (NULL);
 }
-strcpy(name_copy, name);
+for (i = 0; i <= name_len; i++)
+name_copy[i] = name[i];
 
-owner_copy = malloc(strlen(owner) + 1);
+owner_copy = malloc(owner_len + 1);
 if (owner_copy == NULL)
 {
 free(name_copy);
 free(dog);
 return (NULL);
 }
-strcpy(owner_copy, owner);
+for (i = 0; i <= owner_len; i++)
+owner_copy[i] = owner[i];
 
 dog->name = name_copy;
 dog->age = age;
@@ -42,4 +51,3 @@ dog->owner = owner_copy;
 
 return (dog);
 }
-
