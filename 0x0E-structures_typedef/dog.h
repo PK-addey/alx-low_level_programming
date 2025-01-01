d020cb3b6faaa328f4bfa78186de3f86588bcf56
Pk-addey <pkaddey@gmail.com>
2025-01-01 16:36:30 +0000

    Added a README.md file and defined a new type struct dog.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* struct dog - Defines a new type 'struct dog' with elements
+* name, age, and owner.
+*
+* @name: The dog's name.
+* @age: The dog's age.
+* @owner: The dog's owner.
+*/
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog;
