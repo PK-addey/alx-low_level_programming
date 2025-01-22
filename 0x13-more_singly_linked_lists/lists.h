3d0e3b086d2fae2066ebfe3b6a4f18dfbfeca0e1
Pk-addey <pkaddey@gmail.com>
2025-01-22 16:10:55 +0000

    Included function description in lists.h and betty style in 0-print file.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,17 +3,21 @@
 
 #include <stddef.h>
 
-/* Prototype for _putchar */
 int _putchar(char c);
 
-/* Data structure for singly linked list */
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct listint_s
 {
     int n;
     struct listint_s *next;
 } listint_t;
 
-/* Function prototypes for linked list operations */
 size_t print_listint(const listint_t *h);
 
 #endif /* LISTS_H */
